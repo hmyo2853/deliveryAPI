81e3f923da72b43ec793e95d536bba8c1f8e9d4c
hmyo2853 <hmyo2853@gmail.com>
2022-12-02 21:31:04 +0900

    select option 생성, company data mapping

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,35 @@
 import "./App.css";
-import { useState } from "react";
-import { Delivery } from "./sweettracker";
+import { CompanyList, Delivery } from "./sweettracker";
 import { useQuery } from "react-query";
 
 function App() {
   const API_KEY = import.meta.env.VITE_API_KEY;
+  const URL = `http://info.sweettracker.co.kr/api/v1/companylist?t_key=${API_KEY}`;
 
-  /** api 데이터 받아오기 */
-  const getData = async (): Promise<void> => {
-    // company 가져오기
-    // const response = await fetch(
-    //   `http://info.sweettracker.co.kr/api/v1/companylist?t_key=${API_KEY}`
-    // );
-    // const json = await response.json();
-    // console.log(json.Company);
+  /** api company list 데이터 받아오기 */
+  const fetchCompany = async (): Promise<CompanyList[] | void> => {
+    return fetch(URL).then(async (_res) => {
+      if (!_res.ok)
+        throw new Error(`HTTP Error : status code is ${_res.status}`);
+      const _json = await _res.json();
+      const _company = _json.Company as CompanyList[];
+      return _company;
+    });
   };
-  const { data, isLoading, isError } = useQuery("delivery", getData);
 
+  const { data, isLoading, isError } = useQuery("delivery", fetchCompany);
+
+  if (isLoading) return <h2>Loading...</h2>;
   return (
     <div className="App">
-      <h1>내가 편하게 조회하고 싶어서 만든 조회 페이지</h1>
+      <h1>내가 편하게 조회하고 싶어서 만든 택배 송장 조회 페이지</h1>
+      <select>
+        {data?.map((_data, i) => (
+          <option key={i} value={_data.Name}>
+            [{_data.International}], {_data.Code}, {_data.Name}
+          </option>
+        ))}
+      </select>
     </div>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "react-query";
 import App from "./App";
 import "./index.css";
 
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <App />
+  <QueryClientProvider client={queryClient}>
+    <App />
+  </QueryClientProvider>
 );
