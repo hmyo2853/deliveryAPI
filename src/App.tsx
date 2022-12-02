9594b54cad91758852b5b99eee2fc9fbe0cb5e69
hmyo2853 <hmyo2853@gmail.com>
2022-12-02 22:29:40 +0900

    invoice 테스트 및 데이터 확인

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import "./App.css";
-import { CompanyList, Delivery } from "./sweettracker";
+import { CompanyList, Invoice } from "./sweettracker";
 import { useQuery } from "react-query";
 
 function App() {
   const API_KEY = import.meta.env.VITE_API_KEY;
-  const URL = `http://info.sweettracker.co.kr/api/v1/companylist?t_key=${API_KEY}`;
+  /** invoice, company url */
+  const INVOICE_URL = `http://info.sweettracker.co.kr/api/v1/trackingInfo?t_key=${API_KEY}&t_code=04&t_invoice=563295922011`;
+  const COMPANY_URL = `http://info.sweettracker.co.kr/api/v1/companylist?t_key=${API_KEY}`;
 
   /** api company list 데이터 받아오기 */
   const fetchCompany = async (): Promise<CompanyList[] | void> => {
-    return fetch(URL).then(async (_res) => {
+    return fetch(COMPANY_URL).then(async (_res) => {
       if (!_res.ok)
         throw new Error(`HTTP Error : status code is ${_res.status}`);
       const _json = await _res.json();
@@ -17,19 +19,30 @@ function App() {
     });
   };
 
-  const { data, isLoading, isError } = useQuery("delivery", fetchCompany);
+  /** invoice post 데이터 받아오기 */
+  const fetchInvoice = async (): Promise<Invoice[] | void> => {
+    return fetch(INVOICE_URL).then(async (_res) => {
+      if (!_res.ok)
+        throw new Error(`HTTP Error : status code is ${_res.status}`);
+      const _json = await _res.json();
+      console.log(_json);
+    });
+  };
+  // http://info.sweettracker.co.kr/api/v1/trackingInfo?t_code=04?t_invoice=563295922011?t_key=9V5232OctJLmSrSNFZTC7Q
+  // const { data, isLoading, isError } = useQuery("company", fetchCompany);
+  const { data, isLoading, isError } = useQuery("invoice", fetchInvoice);
 
   if (isLoading) return <h2>Loading...</h2>;
   return (
     <div className="App">
       <h1>내가 편하게 조회하고 싶어서 만든 택배 송장 조회 페이지</h1>
-      <select>
+      {/* <select>
         {data?.map((_data, i) => (
           <option key={i} value={_data.Name}>
             [{_data.International}], {_data.Code}, {_data.Name}
           </option>
         ))}
-      </select>
+      </select> */}
     </div>
   );
 }
--- src/sweettracker.d.ts
+++ src/sweettracker.d.ts
@@ -4,6 +4,72 @@ export interface CompanyList {
   Name: string;
 }
 
-export interface Delivery {
-  //
+export interface Invoice {
+  adUrl: string;
+  complete: boolean;
+  invoiceNo: string;
+  itemImage: string;
+  itemName: string;
+  level: number;
+  receiverAddr: string;
+  receiverName: string;
+  recipient: string;
+  result: string;
+  senderName: string;
+  trackingDetails: [
+    {
+      kind: string;
+      level: number;
+      manName: string;
+      manPic: string;
+      telno: string;
+      telno2: string;
+      timeString: string;
+      where: string;
+      code: null;
+      remark: null;
+    }
+  ];
+  orderNumber: null;
+  estimate: string;
+  productInfo: null;
+  zipCode: null;
+  lastDetail: {
+    kind: string;
+    level: number;
+    manName: string;
+    manPic: string;
+    telno: string;
+    telno2: string;
+    timeString: string;
+    where: string;
+    code: null;
+    remark: null;
+  };
+  lastStateDetail: {
+    kind: string;
+    level: number;
+    manName: string;
+    manPic: string;
+    telno: string;
+    telno2: string;
+    timeString: string;
+    where: string;
+    code: null;
+    remark: null;
+  };
+  firstDetail: {
+    kind: string;
+    level: number;
+    manName: string;
+    manPic: string;
+    telno: string;
+    telno2: string;
+    time: number;
+    timeString: string;
+    where: string;
+    code: null;
+    remark: null;
+  };
+  completeYN: string;
 }
